b140f71412e9acfa966e13893d350a316d11b612
Kyle <kyleyoung.9210@gmail.com>
2021-05-26 12:57:13 -0400

    added heroku config for database

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/budget", {
+// connecting to mongodb atlas or local db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  // allowing app to fall back to old parser incase of bug finds
   useNewUrlParser: true,
-  useFindAndModify: false
+  // removes connection options that are no longer relevant with new topology engine
+  useUnifiedTopology: true,
+  // using mongoose default engine build
+  useCreateIndex: true,
+  // using native findOneAndUpdate()
+  useFindAndModify: false,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
