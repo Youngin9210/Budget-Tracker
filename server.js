62751e97d0da092c8eb9239436ff5e12ffaae894
Kyle <kyleyoung.9210@gmail.com>
2021-05-26 13:03:19 -0400

    updated port with process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // connecting to mongodb atlas or local db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   // allowing app to fall back to old parser incase of bug finds
   useNewUrlParser: true,
   // removes connection options that are no longer relevant with new topology engine
