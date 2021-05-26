7c6d7cacc96fd721a65557f14491dda6cb3302cb
Kyle <kyleyoung.9210@gmail.com>
2021-05-26 12:56:48 -0400

    created service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,81 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/manifest.json',
+  '/index.js',
+  '/db.js',
+  '/manifest.webmanifest',
   '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
 ];
+
+const BUDGET_PRECACHE = 'budget-precache-v1';
+const RUNTIME_CACHE = 'runtime';
+
+// install
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches
+      .open(BUDGET_PRECACHE)
+      .then((caches) => {
+        console.log('Your files were pre-cached successfully');
+        return caches.addAll(FILES_TO_CACHE);
+      })
+      .then(self.skipWaiting())
+  );
+});
+
+// activate
+self.addEventListener('active', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== BUDGET_PRECACHE && key !== RUNTIME_CACHE) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener('fetch', function (evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(RUNTIME_CACHE)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(BUDGET_PRECACHE).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
